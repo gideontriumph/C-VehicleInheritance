741d8ac8405e4518b20f1e722ddf894f128ee93b
Faceless <gideontriumph@gmail.com>
2024-05-02 02:46:25 -0600

    Second Commit

--- vehicle.cpp
+++ vehicle.cpp
@@ -11,4 +11,28 @@ private:
     float speed;
     float cargo_capacity;
     bool landed; // To track if the spaceship is landed or not
+
+public:
+    // Constructor
+    Spaceship(string n, float fuel, float s, float capacity) {
+        name = n;
+        fuel_level = fuel;
+        speed = s;
+        cargo_capacity = capacity;
+        landed = false; // Initially, spaceship is not landed
+    }
+
+    // Method to refuel the spaceship
+    void refuel(float amount) {
+        fuel_level += amount;
+        cout << "\033[1;34m" << name << " has been refueled." << endl;
+        cout << "Current fuel level: " << fuel_level << endl;
+    }
+
+    // Method to accelerate the spaceship
+    void accelerate(float increment) {
+        speed += increment;
+        cout << "\033[1;34m" << name << " is now traveling at " << speed << " MPH." << endl;
+    }
+
 };
